a1749850e5d292c512c26cff087e6b88c6ac57ec
Travers La Ville <traverslaville@traverss-mbp.lan>
2023-08-08 23:30:32 -0500

    Added html view to test url routes success

--- todo/todo_api/views.py
+++ todo/todo_api/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+from django.http import HttpResponse #new
+
+def index(request): #new
+    return HttpResponse('<h1>Django Include URLs</h1>')
